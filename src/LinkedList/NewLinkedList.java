96fb7b821fa7a8ee3883e5b1d4ef56d430a92284
manishgautam30 <jatin.agrawal_cs18@gla.ac.in>
2020-02-24 21:45:19 +0530

    creating Performingaction class in which all the methods of operation is to be defined

--- src/LinkedList/NewLinkedList.java
+++ src/LinkedList/NewLinkedList.java
@@ -4,4 +4,17 @@ public class NewLinkedList {
     Person person;
     private Node first;
     private Node last;
-}
+
+    public void addLast(Person person) {
+        Node node = new Node(person);
+        if (isEmpty()) {
+            first = node;
+            last = node;
+        } else {
+            last.setNext(node);
+            last = node;
+
+        }
+
+
+    }
