59dfc5c6836bff630d4d8a90211f7c4802178086
manishgautam30 <jatin.agrawal_cs18@gla.ac.in>
2020-02-24 21:16:02 +0530

    defining the interface of ADT

--- src/LinkedList/Person.java
+++ src/LinkedList/Person.java
@@ -1,4 +1,13 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class Person {
+    private String firstName;
+
+    private String lastName;
+
+    private LinkedList<String> contacts;
+
+    private String email;
 }
